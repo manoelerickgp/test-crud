b5fb68da06398ee4b1513a6a11ef63d2faee4042
manoelerickgp <manoel.computacao@gmail.com>
2023-08-08 23:39:09 -0300

    Adicionando metodo de buscar categoria por id

--- src/main/java/com/estudo/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/estudo/dscatalog/resources/CategoryResource.java
@@ -5,10 +5,7 @@ import com.estudo.dscatalog.entities.Category;
 import com.estudo.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,5 +21,11 @@ public class CategoryResource {
     public ResponseEntity<List<CategoryDTO>> findAll(){
         List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+        CategoryDTO objDTO = service.findById(id);
+        return ResponseEntity.ok().body(objDTO);
     }
 }
