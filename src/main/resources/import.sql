7f5cf2682fcf6895a6f189d8c52aeeec45fadbef
manoelerickgp <manoel.computacao@gmail.com>
2023-08-13 00:07:50 -0300

    Atualizando erro de sintaxe no comando sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category(name, createAt) VALUES ('Books', NOW())
-INSERT INTO tb_category(name, createAt) VALUES ('Eletronics', NOW())
-INSERT INTO tb_category(name, createAt) VALUES ('Computers', NOW())
+INSERT INTO tb_category(name, create_At) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, create_At) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category(name, create_At) VALUES ('Computers', NOW());
