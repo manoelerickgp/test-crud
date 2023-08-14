10d5477f43aca63f493844d875dcdcc41e5c8df9
manoelerickgp <manoel.computacao@gmail.com>
2023-08-13 23:37:45 -0300

    Adicionando paginacao ao metodo de buscar todas as categorias

--- src/main/java/com/estudo/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/estudo/dscatalog/resources/CategoryResource.java
@@ -5,6 +5,9 @@ import com.estudo.dscatalog.entities.Category;
 import com.estudo.dscatalog.services.CategoryService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -22,8 +25,15 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-        List<CategoryDTO> list = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        Page<CategoryDTO> list = service.findAll(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
